c9b21fc3aa84868bdedc508ea661cc70b0b22b87
MaivyAyman <115155818+MaivyAyman@users.noreply.github.com>
2023-05-14 17:12:40 +0300

    Update Main.java

--- src/main/java/com/example/swa7_app/Main.java
+++ src/main/java/com/example/swa7_app/Main.java
@@ -16,10 +16,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         //Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
-        //FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
-        //Parent root = loader.load();
-
-        //Scene scene = new Scene(root, 550, 410);
+        
 
         Parent root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
 
